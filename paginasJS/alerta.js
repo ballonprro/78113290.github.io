0c26bac8fe421088ed65806eba78d282b9fa1a66
CarlosJorge44 <81602514+CarlosJorge44@users.noreply.github.com>
2021-06-18 21:03:35 -0500

    JavascriptCarlos 3

--- paginasJS/alerta.js
+++ paginasJS/alerta.js
@@ -1,2 +1,9 @@
 let nombre = prompt("Bienvenido a nuestra pagina, ¿Como te llamas?");
 alert("Hola" +" " + nombre);
+
+
+function fecha() 
+{
+    var hoy = new Date();
+    document.write(hoy.toDateString());
+}
